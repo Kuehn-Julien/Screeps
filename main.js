8db1eca77ecbbb14704429fd76c195793d5d58c3
Julien Kühn <kuehn.julien@gmail.com>
2016-12-18 21:59:03 +0100

    updated version

--- main.js
+++ main.js
@@ -1,24 +1,43 @@
 var roleHarvester = require('role.harvester');
 var roleBuilder = require('role.builder');
 var roleUpgrader = require('role.upgrader');
+var roleRepairer = require('role.repairer');
 var creator = require('createCreep');
 
 module.exports.loop = function () {
 
-var harvesters = 0;
-var builders = 0;
-var upgraders = 0;
+    // CLEAN UP CREEPS THAT DIED
+    for(var i in Memory.creeps) {
+        if(!Game.creeps[i]) {
+            delete Memory.creeps[i];
+        }
+    }
+
+    var wantedHarvester = 4;
+    var wantedBuilders = 1;
+    var wantedUpgraders = 1;
+    var wantedRepairers = 0;
+
+    var harvesters = 0;
+    var builders = 0;
+    var upgraders = 0;
+    var repairers = 0;
 
     for(var c in Game.creeps){
 
         var creep = Game.creeps[c];
 
+        // Uncomment if you want to stop consuming energy //
+        //if(creep.memory.role != "harvester")
+        //    creep.memory.working = false;
+        // ******************************************** //
+
         switch(creep.memory.role){
             case "harvester": harvesters++; break;
             case "builder": builders++; break;
             case "upgrader": upgraders++; break;
+            case "repairer": repairers++; break;
         }
-
 
         if(creep.memory.role == "harvester")
             roleHarvester.run(creep);
@@ -26,23 +45,43 @@ var upgraders = 0;
             roleBuilder.run(creep);
         else if (creep.memory.role == "upgrader")
             roleUpgrader.run(creep);
+        else if (creep.memory.role == "repairer")
+            roleRepairer.run(creep);
     }
 
-    if(harvesters < 4){
+    if(harvesters < wantedHarvester){
+        console.log("Creating Harvesters right now!");
+
+        for(var c in Game.creeps){
+            var creep = Game.creeps[c];
+            if(creep.memory.role != "harvester")
+                creep.memory.working = false;
+        }
         creator.run("harvester");
     }
-    else
-        console.log("no harvesters needed");
-
-    if(upgraders < 2){
+    else if(upgraders < wantedUpgraders){
+        console.log("Creating Upgraders right now!");
         creator.run("upgrader");
     }
-    else
-        console.log("no upgraders needed");
-
-    if(builders < 2){
+    else if(builders < wantedBuilders){
+        console.log("Creating Builders right now!");
         creator.run("builder");
     }
-    else
-        console.log("no builders needed");
+    else if(repairers < wantedRepairers){
+        console.log("Creating Repairers right now!");
+        creator.run("repairer");
+    }
+
+    if(harvesters >= wantedHarvester){
+        for(var c in Game.creeps){
+            var creep = Game.creeps[c];
+            if(creep.memory.role != "harvester")
+                creep.memory.working = true;
+        }
+    }
+
+    console.log("Harvesters alive: " + harvesters);
+    console.log("Upgraders alive: " + upgraders);
+    console.log("Builders alive: " + builders);
+    console.log("Repairers alive: " + repairers);
 };
