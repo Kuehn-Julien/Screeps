35b431df21d4c90027939a83e01ac04e43c90888
Julien Kühn <kuehn.julien@gmail.com>
2016-12-18 21:57:44 +0100

    updated to the current version

--- createCreep.js
+++ createCreep.js
@@ -3,16 +3,19 @@ module.exports = {
     run: function(arg1){
 
         if(arg1 == "harvester"){
-            var creep = Game.spawns["OverRustle"].createCreep([WORK,CARRY,MOVE,MOVE,MOVE],null,{role:"harvester"});
+            var creep = Game.spawns["OverRustle"].createCreep([WORK,WORK,CARRY,MOVE],null,{role:"harvester",working:true, job_done:false});
         }
         else if(arg1 == "upgrader"){
-            var creep = Game.spawns["OverRustle"].createCreep([WORK,CARRY,MOVE],null,{role:"upgrader"});
+            var creep = Game.spawns["OverRustle"].createCreep([WORK,CARRY,MOVE],null,{role:"upgrader",working:true});
         }
         else if(arg1 == "builder"){
-            var creep = Game.spawns["OverRustle"].createCreep([WORK,CARRY,MOVE],null,{role:"builder"});
+            var creep = Game.spawns["OverRustle"].createCreep([WORK,CARRY,MOVE],null,{role:"builder",working:true});
         }
-
+        else if(arg1 == "claimer"){
+            var creep = Game.spawns["OverRustle"].createCreep([CLAIM, MOVE,]);
+        }
+        else if(arg1 == "repairer"){
+            var creep = Game.spawns["OverRustle"].createCreep([WORK,CARRY,MOVE,MOVE],null,{role:"repairer",working:true});
+        }
     }
-
-
 }
