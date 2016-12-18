3671be229129c2ca6dfa884e3e4ad58ce664a09a
Julien Kühn <kuehn.julien@gmail.com>
2016-12-18 22:00:27 +0100

    Update role.builder.js

--- role.builder.js
+++ role.builder.js
@@ -1,5 +1,5 @@
 module.exports = {
-    
+
     run: function(creep) {
 
 	    if(creep.memory.building && creep.carry.energy == 0) {
@@ -16,22 +16,23 @@ module.exports = {
                     creep.moveTo(targets[0]);
                 }
             }
-
-
 	    } else {
-	        var loc = creep.room.find(FIND_STRUCTURES, {
+
+            if(creep.memory.working == true){
+            var loc = creep.room.find(FIND_STRUCTURES, {
                 filter: (structure) => {
                     return (structure.structureType == STRUCTURE_EXTENSION ||
                             structure.structureType == STRUCTURE_SPAWN ||
                             structure.structureType == STRUCTURE_STORAGE ||
                             structure.structureType == STRUCTURE_CONTAINER ||
-                            structure.structureType == STRUCTURE_TOWER) && structure.energy > structure.energyCapacity / 2;
+                            structure.structureType == STRUCTURE_TOWER);
                 }
             });
 
             if(loc.length != 0){
                 creep.moveTo(loc[0]);
 	            loc[0].transferEnergy(creep);
+            }
             }
 	    }
 	}
