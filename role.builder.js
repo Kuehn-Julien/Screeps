f08b26e6494638e4680e23dcd370407b739c7ddb
Julien Kühn <kuehn.julien@gmail.com>
2016-12-18 23:49:13 +0100

    add afk-flag

--- role.builder.js
+++ role.builder.js
@@ -2,21 +2,24 @@ module.exports = {
 
     run: function(creep) {
 
-	    if(creep.memory.building && creep.carry.energy == 0) {
+        if(creep.memory.building && creep.carry.energy == 0) {
             creep.memory.building = false;
-	    }
-	    if(!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
-	        creep.memory.building = true;
-	    }
+        }
+        if(!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
+            creep.memory.building = true;
+        }
 
-	    if(creep.memory.building) {
-	        var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+        if(creep.memory.building) {
+            var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
             if(targets.length) {
                 if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0]);
                 }
             }
-	    } else {
+            else{
+                creep.moveTo(Game.flags["Afk-Flag"]);
+            }
+        } else {
 
             if(creep.memory.working == true){
             var loc = creep.room.find(FIND_STRUCTURES, {
@@ -31,9 +34,9 @@ module.exports = {
 
             if(loc.length != 0){
                 creep.moveTo(loc[0]);
-	            loc[0].transferEnergy(creep);
+                loc[0].transferEnergy(creep);
             }
             }
-	    }
+        }
 	}
 };
