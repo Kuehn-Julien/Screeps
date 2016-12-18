87a6ef39c99933854faa31cd2e2694de4b0b12d6
Julien Kühn <kuehn.julien@gmail.com>
2016-12-18 22:01:48 +0100

    Update role.harvester.js

--- role.harvester.js
+++ role.harvester.js
@@ -1,14 +1,20 @@
 module.exports = {
     run: function(creep){
-        if(creep.carryCapacity > creep.carry.energy){
+
+        if(creep.carry.energy == creep.carryCapacity)
+            creep.memory.job_done = true;
+        else if (creep.carry.energy == 0)
+            creep.memory.job_done = false;
+
+        if(creep.carryCapacity > creep.carry.energy && creep.memory.job_done != true){
             var source = creep.pos.findClosestByPath(FIND_SOURCES);
             if(creep.harvest(source) == ERR_NOT_IN_RANGE)
                 creep.moveTo(source);
             }
 
-            else{
-                if(creep.transfer(Game.spawns.OverRustle, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)
+        else{
+            if(creep.transfer(Game.spawns.OverRustle, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)
                 creep.moveTo(Game.spawns.OverRustle);
-            }
+        }
     }
 }
