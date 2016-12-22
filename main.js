0a150c8799502e9766460f0dd353afa0a6ca9d02
Kuehn-Julien <kuehn.julien@gmail.com>
2016-12-22 20:45:39 +0100

    clean up main function
    Shrinks some of the code that is needed to build the bots.

--- main.js
+++ main.js
@@ -9,39 +9,31 @@ var creepKiller = require('action.killCreep');
 
 module.exports.loop = function () {
 
-    var wanted_harvester_bots = 0;
-    var wanted_miner_bots = 3;
-    var wanted_builder_bots = 4;
-    var wanted_upgrader_bots = 2;
-    var wanted_repair_bots = 2;
-    var wanted_carry_bots = 3;
-
-    var harvester_bots = 0;
-    var miner_bots = 0;
-    var builder_bots = 0;
-    var upgrader_bots = 0;
-    var repair_bots = 0;
-    var carry_bots = 0;
-
-    // CLEAN UP CREEPS THAT DIED
+    // CLEAN UP CREEPS THAT DIED FROM MEMORY
     for(var i in Memory.creeps) {
         if(!Game.creeps[i]) {
             delete Memory.creeps[i];
         }
     }
 
+    // Harvest->Miner->Carry->Upgrader->Builder->Repair
+    var creepType = ["harvester_bot", "miner_bot", "carry_bot",
+                            "upgrader_bot", "builder_bot", "repair_bot"];
+    var creepsNow = [0,0,0,0,0,0];
+    var creepsNeeded = [1,0,0,0,0,0];
+
     for(var c in Game.creeps){
 
         var creep = Game.creeps[c];
 
-        if((creep.ticksToLive >= 65) && !creep.spawning){
+        if((creep.ticksToLive >= 50) && !creep.spawning){
             switch(creep.memory.role){
-                case "harvester_bot": harvester_bots++; break;
-                case "miner_bot": miner_bots++; break;
-                case "builder_bot": builder_bots++; break;
-                case "upgrader_bot": upgrader_bots++; break;
-                case "repair_bot": repair_bots++; break;
-                case "carry_bot": carry_bots++; break;
+                case "harvester_bot": creepsNow[0]++; break;
+                case "miner_bot": creepsNow[1]++; break;
+                case "carry_bot": creepsNow[2]++ break;
+                case "upgrader_bot": creepsNow[3]++; break;
+                case "builder_bot": creepsNow[4]++; break;
+                case "repair_bot": creepsNow[5]++; break;
             }
 
             if(creep.memory.role == "harvester_bot")
@@ -63,41 +55,13 @@ module.exports.loop = function () {
 
     }
 
-    if(carry_bots < wanted_carry_bots){
-        console.log("Creating carry_bots right now!");
-        console.log("Currently there are " + carry_bots + " carry_bots alive!");
-        creepBuilder.run("carry_bot");
-    }
-    else if(miner_bots < wanted_miner_bots){
-        console.log("Creating miner_bots right now!");
-        console.log("Currently there are " + miner_bots + " miner_bots alive!");
-        creepBuilder.run("miner_bot");
-    }
-    else if(harvester_bots < wanted_harvester_bots){
-        console.log("Creating harvester_bot right now!");
-        console.log("Currently there are " + harvester_bots + " harvester_bots alive!");
-        creepBuilder.run("harvester_bot");
-    }
-    else if(upgrader_bots < wanted_upgrader_bots){
-        console.log("Creating upgrader_bot right now!");
-        console.log("Currently there are " + upgrader_bots + " upgrader_bots alive!");
-        creepBuilder.run("upgrader_bot");
-    }
-    else if(repair_bots < wanted_repair_bots){
-        console.log("Creating repair_bot right now!");
-        console.log("Currently there are " + repair_bots + " repair_bots alive!");
-        creepBuilder.run("repair_bot");
-    }
-    else if(builder_bots < wanted_builder_bots){
-        console.log("Creating builder_bot right now!");
-        console.log("Currently there are " + builder_bots + " builder_bots alive!");
-        creepBuilder.run("builder_bot");
-    }
-
-    if((miner_bots >= wanted_miner_bots/2) && (carry_bots >= wanted_carry_bots/2)){
-        for(var c in Game.creeps){
-            var creep = Game.creeps[c];
-            creep.memory.interrupt = false;
+    // Build creeps
+    for(var x=0; x<creepsNeeded; x++){
+        if(creepsNow[x] < creepsNeeded[x]){
+            console.log("Building " + creepType[x] " right now!");
+            creepBuilder.run(creepType[x]);
+            break;
         }
     }
+
 }
